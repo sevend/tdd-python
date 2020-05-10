d361c3a9db3a1218cba3a08ac32e77a652262c51
Seven <enter775@gmail.com>
2020-05-10 22:06:13 +0800

    setting ALLOWHOST

--- superlists/settings.py
+++ superlists/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'nxtbx6mq73ltf7iuthcan9md)495&xw$p#zd@3!f1$a&wbe)7i'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [192.168.1.27]
 
 
 # Application definition
