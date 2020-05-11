9e4c8e1923e297c8e595a1d69f30eb02539f30dd
Seven <enter775@gmail.com>
2020-05-11 11:08:28 +0800

    fabfile.py test1

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -1,4 +1,4 @@
-from fabric.contrib.files import append, exists, sed
+from contrib.files import append, exists, sed
 from fabric.api import env, local, run
 
 import random
@@ -20,37 +20,37 @@ def _create_directory_structure_if_necessary(site_folder):
         run('mkdir -p %s/%s' % (site_folder, subfolder))
 
 def _get_latest_source(source_folder):
-    if exists(source_folder + '/.git'): # ➊
-        run('cd %s && git fetch' % (source_folder,)) # ➋➌
+    if exists(source_folder + '/.git'): 
+        run('cd %s && git fetch' % (source_folder,)) 
     else:
-        run('git clone %s %s' % (REPO_URL, source_folder)) # ➍
-    current_commit = local( " git log -n 1 --format=%H " , capture=True) # ➎
-    run('cd %s && git reset --hard %s' % (source_folder, current_commit)) # ➏
+        run('git clone %s %s' % (REPO_URL, source_folder))
+    current_commit = local( " git log -n 1 --format=%H " , capture=True)
+    run('cd %s && git reset --hard %s' % (source_folder, current_commit))
 
 def _update_settings(source_folder, site_name):
     settings_path = source_folder + '/superlists/settings.py'
-    sed(settings_path, " DEBUG = True " , " DEBUG = False " ) # ➊
+    sed(settings_path, " DEBUG = True " , " DEBUG = False " )
     sed(settings_path,
         'ALLOWED_HOSTS =.+$',
-        'ALLOWED_HOSTS = [ " %s " ]' % (site_name,) # ➋
+        'ALLOWED_HOSTS = [ " %s " ]' % (site_name,)
     )
     secret_key_file = source_folder + '/superlists/secret_key.py'
-    if not exists(secret_key_file): # ➌
+    if not exists(secret_key_file):
         chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
         key = ''.join(random.SystemRandom().choice(chars) for _ in range(50))
         append(secret_key_file, " SECRET_KEY = '%s' " % (key,))
-    append(settings_path, '\nfrom .secret_key import SECRET_KEY') # ➍➎
+    append(settings_path, '\nfrom .secret_key import SECRET_KEY')
 
 def _update_virtualenv(source_folder):
     virtualenv_folder = source_folder + '/../virtualenv'
-    if not exists(virtualenv_folder + '/bin/pip'): # ➊
+    if not exists(virtualenv_folder + '/bin/pip'):
         run('virtualenv --python=python3 %s' % (virtualenv_folder,))
-    run('%s/bin/pip install -r %s/requirements.txt' % ( # ➋
+    run('%s/bin/pip install -r %s/requirements.txt' % (
         virtualenv_folder, source_folder
     ))
 
 def _update_static_files(source_folder):
-    run('cd %s && ../virtualenv/bin/python3 manage.py collectstatic --noinput' % ( # ➊
+    run('cd %s && ../virtualenv/bin/python3 manage.py collectstatic --noinput' % (
         source_folder,
     ))
 
