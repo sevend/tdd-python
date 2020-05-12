de036bc602c1fdb0ad019a824aac7429a8fb01f5
Seven <enter775@gmail.com>
2020-05-12 15:25:16 +0800

    Implement duplicate item validation at model layer

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,5 +1,5 @@
-
 from .base import FunctionalTest
+from selenium.webdriver.common.keys import Keys
 from unittest import skip
 import time
 
@@ -10,6 +10,7 @@ class ItemValidationTest(FunctionalTest):
         # 输入框中没输入内容，她就按下了回车键
         self.browser.get(self.server_url)
         self.get_item_input_box().send_keys('\n')
+        self.get_item_input_box().send_keys(Keys.ENTER)
 
 
         # 首页刷新了，显示一个错误消息
@@ -21,10 +22,12 @@ class ItemValidationTest(FunctionalTest):
 
         # 她输入一些文字，然后再次提交，这次没问题了
         self.get_item_input_box().send_keys('Buy milk\n')
+        self.get_item_input_box().send_keys(Keys.ENTER)
         self.check_for_row_in_list_table('1: Buy milk')
 
         # 她有点儿调皮，又提交了一个空待办事项
         self.get_item_input_box().send_keys('\n')
+        self.get_item_input_box().send_keys(Keys.ENTER)
 
         # 在清单页面她看到了一个类似的错误消息
         self.check_for_row_in_list_table('1: Buy milk')
@@ -36,3 +39,20 @@ class ItemValidationTest(FunctionalTest):
         self.check_for_row_in_list_table('1: Buy milk')
         self.check_for_row_in_list_table('2: Make tea')
         
+    def test_cannot_add_duplicate_items(self):
+        # 伊迪丝访问首页，新建一个清单
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys('Buy wellies\n')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy wellies')
+
+        # 她不小心输入了一个重复的待办事项
+        self.get_item_input_box().send_keys('Buy wellies\n')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        # 她看到一条有帮助的错误消息
+        self.check_for_row_in_list_table('1: Buy wellies')
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, " You've already got this in your list " )
